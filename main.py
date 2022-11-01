a496fe3723ae3b23208d4b9158b931d8cd80fb5f
Careca Voador <e.rodrigo@outlook.com>
2022-11-01 16:59:54 -0300

    Bugfixes.

--- main.py
+++ main.py
@@ -1,16 +1,19 @@
+import sys
+
 from pathlib import Path
 from decimal import Decimal
+from copy import copy
 
 from PyPDF2 import PdfReader, PdfWriter, PageObject, Transformation
-from PyPDF2.generic import RectangleObject
 
 
 # 72 pontos por polegada (resolução padrão de um arquivo .PDF)
 # 1 pol == 25.4 mm
 POL = Decimal(72)
 MM = POL / Decimal(25.4)
-LARGURA_ROLO = 2592
 ALTURA_MAX = 1200 * MM
+LARGURA_ROLO = 2592
+MARGEM = float(20 * MM)
 
 
 def sort_paginas(_pagina: PageObject) -> Decimal:
@@ -18,9 +21,10 @@ def sort_paginas(_pagina: PageObject) -> Decimal:
     return _pagina.mediabox.height
 
 
-def atualiza_boxes(_pagina: PageObject, novo_retangulo: RectangleObject) -> PageObject:
+def atualiza_boxes(_pagina: PageObject, novo_retangulo: tuple[float, float, float, float]) -> PageObject:
     """Atualiza todos os boxes de uma página com o retângulo informado."""
     _pagina.update({"/MediaBox": novo_retangulo})
+    _pagina.update({"/TrimBox": novo_retangulo})
     _pagina.update({"/TrimBox": novo_retangulo})
     _pagina.update({"/ArtBox": novo_retangulo})
     _pagina.update({"/BleedBox": novo_retangulo})
@@ -41,7 +45,7 @@ def orienta_pagina(_pagina: PageObject, vertical: bool = True) -> PageObject:
             .rotate(90)
             .translate(altura / 2, largura / 2,)
         )
-        _pagina = atualiza_boxes(_pagina, RectangleObject([0, 0, altura, largura]))
+        _pagina = atualiza_boxes(_pagina, (0, 0, altura, largura))
         pagina_rotacionada.merge_page(_pagina)
         return pagina_rotacionada
     return _pagina
@@ -49,39 +53,53 @@ def orienta_pagina(_pagina: PageObject, vertical: bool = True) -> PageObject:
 
 def main():
     """Início do programa."""
-    lista_pdfs = Path("pdfs_exemplo").iterdir()
-    lista_paginas = [PdfReader(pdf).pages[0] for pdf in lista_pdfs]
-    lista_paginas = [orienta_pagina(pagina) for pagina in lista_paginas]
+    argumentos = sys.argv[1:]
+    # lista_pdfs = Path("pdfs_exemplo").iterdir()
+    if argumentos:
+        lista_pdfs = [Path(pdf) for pdf in argumentos if Path(pdf).suffix.lower() == '.pdf']
+        lista_paginas = [PdfReader(pdf).pages[0] for pdf in lista_pdfs]
+        lista_paginas = [orienta_pagina(pagina) for pagina in lista_paginas]
+    else:
+        sys.exit()
 
+    dir_saida = lista_pdfs[0].parent
+    
     # Ordena a lista de págidas da maior para a menor.
     paginas_ordenadas = sorted(lista_paginas, key=sort_paginas, reverse=True)
 
-    # Cria uma nova página com a largura do rolo e a altura da maior página.
-    altura = min(ALTURA_MAX, paginas_ordenadas[0].mediabox.height)
-    nova_pagina = PageObject().create_blank_page(None, LARGURA_ROLO, altura)
 
-    largura_total = 0.0
+    num_paginas = 0
+    
+    while len(paginas_ordenadas) > 0:       
+        # Cria uma nova página com a largura do rolo e a altura da maior página.
+        altura = min(ALTURA_MAX, paginas_ordenadas[0].mediabox.height)
+        nova_pagina = PageObject().create_blank_page(None, float(LARGURA_ROLO), float(altura))
 
-    for idx, pagina in enumerate(paginas_ordenadas):
-        largura_pagina, altura_pagina = float(pagina.mediabox.width), float(pagina.mediabox.height)
-        if idx == 0:
-            # Se for a primeira página, não aplica nenhuma transformação. Somente mescla na coordenada 0, 0.
-            nova_pagina.merge_page(pagina)
-        else:
-            # Aplica a transformação para as páginas subsequentes (se houver).
-            pagina.add_transformation(
-                Transformation()
-                .translate(tx=largura_total, ty=0)
-            )
-            pagina = atualiza_boxes(pagina, RectangleObject([largura_total, 0, largura_total + largura_pagina, altura_pagina]))
-            nova_pagina.merge_page(pagina)
-        largura_total += largura_pagina
+        largura_total = 0.0
+        
+        for idx, pagina in enumerate(copy(paginas_ordenadas)):
+            largura_pagina, altura_pagina = float(pagina.mediabox.width), float(pagina.mediabox.height)
+            if (largura_total + largura_pagina + MARGEM) > LARGURA_ROLO:
+                continue
+            if idx == 0:
+                # Se for a primeira página, não aplica nenhuma transformação. Somente mescla na coordenada 0, 0.
+                nova_pagina.merge_page(pagina, expand=True)
+            else:
+                # Aplica a transformação para as páginas subsequentes (se houver).
+                pagina.add_transformation(
+                    Transformation()
+                    .translate(tx=largura_total, ty=0)
+                )
+                pagina = atualiza_boxes(pagina, (largura_total, 0, largura_total + largura_pagina, altura_pagina))
+                nova_pagina.merge_page(pagina, expand=True)
+            largura_total += largura_pagina + MARGEM
+            paginas_ordenadas.remove(pagina)
 
-    writer = PdfWriter()
-    writer.add_page(nova_pagina)
-
-    with open("novo.pdf", "wb") as novo:
-        writer.write(novo)
+        writer = PdfWriter()
+        writer.add_page(nova_pagina)
+        num_paginas += 1
+        with open(dir_saida.joinpath(f"pg_rolo_{num_paginas}.pdf"), "wb") as novo:
+            writer.write(novo)
 
 
 if __name__ == "__main__":
